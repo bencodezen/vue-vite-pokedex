5e574c7c6886f448b3d4594f0de3d14489b90425
Ben Hong <ben@bencodezen.io>
2021-07-27 12:35:17 -0400

    refactor: use reactive store instead

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,35 @@
   <h1>Twitch Pokedex</h1>
   <input type="text" v-model="filterText" />
   <ul>
-    <li v-for="(pokemon, index) in filteredPokemonList" :key="`poke-${index}`">
+    <li
+      v-for="(pokemon, index) in pokemonStore.filteredList"
+      :key="`poke-${index}`"
+    >
       #{{ pokemon.entry_number }} - {{ pokemon.pokemon_species.name }}
     </li>
   </ul>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue'
+import { reactive, ref, computed, onMounted } from 'vue'
 
-const pokemonList = ref([])
 const filterText = ref('')
 
-const filteredPokemonList = computed(() =>
-  pokemonList.value.filter(pokemon =>
-    pokemon.pokemon_species.name.includes(filterText.value)
+const pokemonStore = reactive({
+  list: [],
+  filteredList: computed(() =>
+    pokemonStore.list.filter(pokemon =>
+      pokemon.pokemon_species.name.includes(filterText.value)
+    )
   )
-)
+})
 
 onMounted(async () => {
   const pokeData = await fetch('https://pokeapi.co/api/v2/pokedex/2/').then(
     response => response.json()
   )
 
-  pokemonList.value = pokeData.pokemon_entries
+  pokemonStore.list = pokeData.pokemon_entries
 })
 </script>
 
