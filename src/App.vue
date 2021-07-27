7a8880b3d441e06d5a32c699708cbb5b1df46e6c
Ben Hong <ben@bencodezen.io>
2021-07-27 12:30:09 -0400

    feature: add pokedex with filtered list

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>Twitch Pokedex</h1>
+  <input type="text" v-model="filterText" />
+  <ul>
+    <li v-for="(pokemon, index) in filteredPokemonList" :key="`poke-${index}`">
+      #{{ pokemon.entry_number }} - {{ pokemon.pokemon_species.name }}
+    </li>
+  </ul>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed, onMounted } from 'vue'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+const pokemonList = ref([])
+const filterText = ref('')
+
+const filteredPokemonList = computed(() =>
+  pokemonList.value.filter(pokemon =>
+    pokemon.pokemon_species.name.includes(filterText.value)
+  )
+)
+
+onMounted(async () => {
+  const pokeData = await fetch('https://pokeapi.co/api/v2/pokedex/2/').then(
+    response => response.json()
+  )
+
+  pokemonList.value = pokeData.pokemon_entries
+})
 </script>
 
 <style>
